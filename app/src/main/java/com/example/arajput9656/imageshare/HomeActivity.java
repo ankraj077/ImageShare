6b34f630ca1304f3a5da8a62139b1feca6d31a20
Arajput9656 <ankitrajput077@hotmail.com>
2017-02-10 12:31:05 -0500

    Code to open the Gallery on button click.

--- app/src/main/java/com/example/arajput9656/imageshare/HomeActivity.java
+++ app/src/main/java/com/example/arajput9656/imageshare/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.arajput9656.imageshare;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,17 +12,24 @@ public class HomeActivity extends AppCompatActivity {
 
     ImageView imageView;
     Button button_get_image;
+    private String selectedImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        imageView = (ImageView)findViewById(R.id.imageView);
-        button_get_image = (Button)findViewById(R.id.button_get_image);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button_get_image = (Button) findViewById(R.id.button_get_image);
     }
 
-    public void getImage(View view){
-
+    public void getImage(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Select Picture"), 1);
     }
+
+    
 }
